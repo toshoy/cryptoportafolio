b36c0f97e9546b5c295d39bc887619883f7f554b
toshoy <cesarrodriguez10@gmail.com>
2021-04-22 10:17:13 -0500

    pruebafinal

--- telegram-bot.py
+++ telegram-bot.py
@@ -61,7 +61,7 @@ def dataframe():
 if __name__ == '__main__':
 
 
-    schedule.every().day.at("8:00").do(dataframe)
+    schedule.every().day.at("08:00").do(dataframe)
     schedule.every().day.at("15:00").do(dataframe)
     schedule.every().day.at("21:00").do(dataframe)
 
