f1dfd40681587df8e24ac89bd4ac6cf5a3d659c0
toshoy <cesarrodriguez10@gmail.com>
2021-04-22 15:08:45 -0500

    ultimo

--- telegram-bot.py
+++ telegram-bot.py
@@ -59,12 +59,10 @@ def dataframe():
 
 
 if __name__ == '__main__':
+    dataframe()
+    # schedule.every().day.at("08:00").do(dataframe)
+    # schedule.every().day.at("15:00").do(dataframe)
+    # schedule.every().day.at("21:00").do(dataframe)
 
-
-    schedule.every().day.at("08:00").do(dataframe)
-    schedule.every().day.at("15:00").do(dataframe)
-    schedule.every().day.at("21:00").do(dataframe)
-
-    while True:
-        schedule.run_pending()
-
+    # while True:
+    #     schedule.run_pending()
