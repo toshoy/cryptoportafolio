1fd7f4e58a648d86e6ecb857715a9b87b82033ef
toshoy <cesarrodriguez10@gmail.com>
2021-05-05 10:23:10 -0500

    prueba1

--- telegram-bot.py
+++ telegram-bot.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 import schedule
+import os
+from os import environ
 
 CURRENCY = ['Binance Coin','Litecoin', 'Uniswap', 'Solana', 'Cardano', 'VeChain', 'PancakeSwap', 'Polygon', 'Chainlink','Fantom', 'Swipe', 'Polygon', 'Dogecoin' ]
 SYMBOLS = ['BNB','LTC','UNI', 'SOL', 'ADA', 'VET','CAKE', 'MATIC' ,'LINK','FTM', 'SXP', 'MATIC', 'DOGE']
@@ -12,8 +14,8 @@ AMOUNT = [2.226,1.98,7.465,6.69,160.4,407,5.424,187,5.80,126,20.471,141.40, 184.
 
 def bot_send_text(bot_message):
 
-      bot_token = '1631795024:AAHwTHG7xWBcIACxBaiLSp0bDenbYkF5414'
-      bot_chatID = '1139403272'
+      bot_token = environ['bot_token']
+      bot_chatID = environ['bot_chatID']
       send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
       response = requests.get(send_text)
       return response
@@ -61,9 +63,11 @@ def dataframe():
 
 if __name__ == '__main__':
 
-    schedule.every().day.at("12:30").do(dataframe)
-    schedule.every().day.at("20:00").do(dataframe)
-    schedule.every().day.at("02:35").do(dataframe)
+    dataframe()
 
-    while True:
-        schedule.run_pending()
+    # schedule.every().day.at("12:30").do(dataframe)
+    # schedule.every().day.at("20:00").do(dataframe)
+    # schedule.every().day.at("02:35").do(dataframe)
+
+    # while True:
+    #     schedule.run_pending()
