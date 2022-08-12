1663e61b9f8981d9a9067fa44ec3e67c8e4fbb6b
toshoy <cesarrodriguez10@gmail.com>
2022-08-12 09:39:13 -0500

    2

--- telegram-bot.py
+++ telegram-bot.py
@@ -27,6 +27,7 @@ def run():
     for i in CURRENCY:
         i = i.lower().strip().replace(' ','-')
         url = requests.get('https://coinmarketcap.com/currencies/'+i+'/')
+        
         soup = BeautifulSoup(url.content, 'html.parser')
         price = soup.find('div',{'class': 'priceValue'}).text
         price = price.strip()
